d53dbd921fe7aed80b87ccb92d9948cc87f7c3d3
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-07 16:06:15 +0000

    function added

--- array-map.js
+++ array-map.js
@@ -3,7 +3,10 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(object) {
+  return Math.sqrt(Math.pow(object.x, 2) + Math.pow(object.y, 2));
+});
+
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
