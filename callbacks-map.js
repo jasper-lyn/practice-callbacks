c5f2c5533a6ed859d3ecb65ca62d00344a2b618b
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-07 19:23:04 +0000

    adds new map function

---
+++ callbacks-map.js
@@ -0,0 +1,54 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+
+function map(array, fn) {
+  let newArray = []
+
+  function pushToNewArr(e) {
+    newArray.push(fn(e))
+  }
+
+  array.forEach(pushToNewArr);
+
+// array.forEach(function(e) {
+//     newArray.push(fn(e))
+//   });
+
+  //for (i = 0; i < array.length; i++) {
+  //newArray.push(fn(array[i]))
+  //}
+  return newArray
+}
+
+const lengthOfWords = map(words, function(word) {
+  return word.length;
+});
+
+const upperCaseWords = map(words, function(word) {
+  return word.toUpperCase();
+});
+
+const reverseWords = map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+
+
+
+console.log(lengthOfWords)
+console.log(upperCaseWords)
+console.log(reverseWords)
+
+console.log(lengthOfWords.toString() + '\n' + 'other string')
+
+
+//console.log(`${lengthOfWords}
+//${upperCaseWords}
+//${reverseWords}`)
+
+
+//EXPECTED OUTPUT
+//[6, 7, 2, 5, 3]
+//[ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
+//[ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
+
